87c455d6f9293da15633eb52d019013f39c8ad47
Felix Donkers <14913523+felixdonkers@users.noreply.github.com>
2022-02-06 16:47:18 +0100

    minor

--- ntp.py
+++ ntp.py
@@ -3,7 +3,7 @@ import time
 from time import ctime
 
 c = ntplib.NTPClient()
-
+ 
 # https://gist.github.com/mutin-sa/eea1c396b1e610a2da1e5550d94b0453
 ntp_servers = ["time.esa.int", "time.nist.gov", "pool.ntp.org", "ntp.time.nl", "time.google.com", "time.facebook.com", "time.apple.com", "time.windows.com"]
 ntp_errors = [0] * len(ntp_servers)
